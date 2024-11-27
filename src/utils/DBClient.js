9a8221279cc128f5e0d4b9b1519bdaaee39c2441
TodoBugado <rafaelsouzaaguiar10@gmail.com>
2024-11-27 15:15:39 -0300

    teste heroku

--- src/utils/DBClient.js
+++ src/utils/DBClient.js
@@ -3,6 +3,5 @@ import axios from 'axios'
 axios.defaults.withCredentials = true;
 
 export default axios.create({
-    baseURL: 'http://localhost:8081'
-    //'https://datavisa-a95e5e4eec8d.herokuapp.com'
+    baseURL: 'https://datavisa-a95e5e4eec8d.herokuapp.com'//'http://localhost:8081'
 })
